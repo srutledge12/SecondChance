6bec05b35c6b92069c5099f34915f44ebecb4c4a
LADent99 <LADent99@gmail.com>
2022-02-01 17:35:20 -0500

    amplify script

---
+++ amplify.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+IFS='|'
+args=("$@")
+
+appId=""
+envName=""
+useProfile="true"
+profileName="default"
+accessKey=""
+secretKey=""
+for ((argPos=0; argPos <= ${#args}; argPos++))
+do
+  if [ "${args[$argPos]}" == "--appid" ]; then
+    appId="${args[(($argPos+1))]}"
+    argPos=$((argPos+1))
+  elif [ "${args[$argPos]}" == "--envName" ]; then
+    envName="${args[(($argPos+1))]}"
+    argPos=$((argPos+1))
+  elif [ "${args[$argPos]}" == "--accessKey" ]; then
+    accessKey="${args[(($argPos+1))]}"
+    argPos=$((argPos+1))
+  elif [ "${args[$argPos]}" == "--secretKey" ]; then
+    secretKey="${args[(($argPos+1))]}"
+    argPos=$((argPos+1))
+  fi
+done
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":${useProfile},\
+\"accessKeyId\":${accessKey},\
+\"secretAccessKey\":${secretKey},\
+\"profileName\":\"${profileName}\"\
+}"
+AMPLIFY="'{\
+\"appId\":\"${appId}\",\
+\"envName\":\"${envName}\"\
+}'"
+PROVIDERS="'{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}'"
+
+cmd="amplify pull --providers ${PROVIDERS} --amplify ${AMPLIFY} --yes"
+echo $cmd
+eval $cmd
